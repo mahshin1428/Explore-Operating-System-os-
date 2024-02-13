22025f4f7990e42bf2454595a8f737fb5df1165b
Mahshin Islam <126387570+mahshin1428@users.noreply.github.com>
2024-02-13 10:27:49 +0600

    Add files via upload

---
+++ c_process.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(){
+int pid;
+
+switch(pid = fork()){
+
+case 0:
+printf(" I am the child process: pid=%d\n",getpid());
+break;
+
+default:
+printf("I am the parent process: pid= %d,child pid = %d\n",getpid(),pid);
+break;
+
+case -1 :
+perror("frok");
+exit(1);
+}
+exit(0);
+} 
